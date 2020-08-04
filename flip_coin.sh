9a7ab42cf3e046971cd26c01316a5859a078e106
gokul-sarath07 <gokul510.s@gmail.com>
2020-08-05 00:55:03 +0530

    Total Times Head and Tail Won

--- flip_coin.sh
+++ flip_coin.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 echo "Coin Flip Simulation"
-check=$(( RANDOM%2 ))
-if [[ $check -eq 1 ]]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+for i in {1..10}
+do
+	check=$(( RANDOM%2 ))
+	if [[ $check -eq 1 ]]
+	then
+		ctHead=$(( $ctHead + 1 ))
+	else
+		ctTail=$(( $ctTail + 1 ))
+	fi
+done
+echo "Total Head: "$ctHead
+echo "Total Tail: "$ctTail
