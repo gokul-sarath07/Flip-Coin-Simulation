001d7df9f213ce494ae94bf6b55f9f53f7c5c2b3
gokul-sarath07 <gokul510.s@gmail.com>
2020-08-06 23:52:02 +0530

    The Tie Condition

--- flip_coin.sh
+++ flip_coin.sh
@@ -1,22 +1,43 @@
-#!/bin/bash -x
+#!/bin/bash
 echo "Coin Flip Simulation"
+function HeadorTail() {
+	case $1 in
+	1)
+		ctHead=$(( $ctHead + 1 ))
+		;;
+	0)
+		ctTail=$(( $ctTail + 1 ))
+		;;
+	esac
+}
 for i in {1..43}
 do
-	check=$(( RANDOM%2 ))
-	if [[ $check -eq 1 ]]
+	HeadorTail $(( RANDOM%2 ))
+	if [[ $ctHead -eq 21 && $ctTail -eq 21 ]]
 	then
-		ctHead=$(( $ctHead + 1 ))
-	else
-		ctTail=$(( $ctTail + 1 ))
-	fi
-	if [[ $ctHead -eq 21 || $ctTail -eq 21 ]]
+		for i in {1..3}
+		do
+			HeadorTail $(( RANDOM%2 ))
+			if [[ $(( $ctHead - $ctTail )) -eq 2 ]]
+			then
+				echo "Head Won!!"
+				exit
+			elif [[ $(( $ctTail - $ctHead )) -eq 2 ]]
+			then
+				echo "Tail Won!!"
+				exit
+			fi
+		done
+	elif [[ $ctHead -gt 21 || $ctTail -gt 21 ]]
 	then
-		break
+		if [[ $ctHead > $ctTail ]]
+		then
+    		echo "Heads have won by "$(( $ctHead - $ctTail ))
+			exit
+		elif [[ $ctTail > $ctHead ]]
+		then
+    		echo "Tails have won by "$(( $ctTail - $ctHead ))
+			exit
+		fi
 	fi
 done
-if [[ $ctHead -gt $ctTail ]]
-then
-	echo "Heads have won by "$(( $ctHead - $ctTail ))
-else
-	echo "Tails have won by "$(( $ctTail - $ctHead ))
-fi
